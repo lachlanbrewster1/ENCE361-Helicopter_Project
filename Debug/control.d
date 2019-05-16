485741de16d272fd61173bdea1fa68de9a3958e3
Ben Slattery <bsl28@canterbury.ac.nz>
2019-05-16 20:30:50 +1200

    deleted everything

--- Debug/control.d
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-control.obj: ../control.c
-control.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.9.LTS/include/stdint.h
-control.obj: ../control.h
-
-../control.c: 
-C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.9.LTS/include/stdint.h: 
-../control.h: 
